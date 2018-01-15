1e5b357323b79dd252df917fb02cdc92f1331b74
Bo Wang <Bobbie.Wang@gmail.com>
2018-01-15 14:25:08 +0800

    Initial version.

---
+++ src/base/klass/i/klass.h
@@ -0,0 +1,6 @@
+class Klass
+{
+public:
+    void normal_member_function();
+    void virtual_member_function();
+};
---
+++ src/base/klass/s/klass.cpp
@@ -0,0 +1,9 @@
+#include "klass.h"
+
+void Klass::normal_member_function()
+{
+}
+
+void Klass::virtual_member_function()
+{
+}
